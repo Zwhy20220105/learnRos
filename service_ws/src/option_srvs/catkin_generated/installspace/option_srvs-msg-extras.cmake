13d78fa31502e18876068f2745a3cca281e90816
zwhy2022 <zhaojun@iplusbot.com>
2023-03-04 20:37:26 +0800

    yes

--- service_ws/src/option_srvs/catkin_generated/installspace/option_srvs-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(option_srvs_MESSAGE_FILES "")
-set(option_srvs_SERVICE_FILES "srv/NumOption.srv")
--- service_ws/src/option_srvs/catkin_generated/installspace/option_srvs-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.installspace.in
-
-_prepend_path("${option_srvs_DIR}/.." "" option_srvs_MSG_INCLUDE_DIRS UNIQUE)
-set(option_srvs_MSG_DEPENDENCIES std_msgs)
